913b72fa09a3306158f3b419f21d91b2c95e102d
akosbujdi <b00153898@mytudublin.ie>
2023-12-15 15:54:52 +0000

    challenge 4 build

---
+++ Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500;
+    private GameObject focalPoint;
+
+    public ParticleSystem smokeParticle;
+    private bool turboAvailable = true;
+    private float turboMultiplier = 7.0f;
+    private float turboWait = 3.0f;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        smokeParticle = GameObject.Find("Smoke_Particle").GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+        smokeParticle.transform.position = new Vector3(transform.position.x, -0.5f, transform.position.z);
+
+        if (Input.GetKeyDown(KeyCode.Space) && turboAvailable)
+        {
+            turboAvailable = false;
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * turboMultiplier * Time.deltaTime, ForceMode.Impulse);
+            smokeParticle.Play();
+            StartCoroutine(TurboCooldown());
+        } else
+        {
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        }
+
+        
+    }
+    IEnumerator TurboCooldown()
+    {
+        yield return new WaitForSeconds(turboWait);
+        turboAvailable = true;
+        smokeParticle.Stop();
+    }
+
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
